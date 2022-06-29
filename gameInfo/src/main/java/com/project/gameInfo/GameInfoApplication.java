fc9c10319fb2c891937e5eb5c1b084e1f7686cf2
MoMoon-LKH <rlgh28@naver.com>
2022-06-29 12:59:58 +0900

    Initial Project

---
+++ gameInfo/src/main/java/com/project/gameInfo/GameInfoApplication.java
@@ -0,0 +1,13 @@
+package com.project.gameInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameInfoApplication.class, args);
+	}
+
+}
