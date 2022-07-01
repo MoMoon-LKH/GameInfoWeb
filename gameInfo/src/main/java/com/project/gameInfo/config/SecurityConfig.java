bcb4f819f08f2dd884e14ceb60a9627edf074139
MoMoon-LKH <rlgh28@naver.com>
2022-07-01 14:39:15 +0900

    AWS DB 연결 확인

---
+++ gameInfo/src/main/java/com/project/gameInfo/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.project.gameInfo.config;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+
+public class SecurityConfig extends SecurityConfigurerAdapter {
+
+
+}
